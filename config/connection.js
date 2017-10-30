e73e2bc4c54b6a53d83fa852b2f59939b76a3c86
Mike Fuchsman <mike.fuchsman@summitmediadesign.com>
2017-10-30 13:29:18 -0600

    Fixed Routes

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,24 @@
 var mySQL = require('mysql');
 
-var connection = mySQL.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
+// var connection = mySQL.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'burgers_db'
+// });
 
-// if (process.env.JAWSDB_URL) {
-//     connection = mySQL.createConnection(process.env.JAWSDB_URL);
-// } else{
-//     connection = mySQL.createConnection({
-//         port: 3306,
-//         host: 'localhost',
-//         user: 'root',
-//         password: '',
-//         database: 'burgers_db',
-//     });
-// }
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mySQL.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mySQL.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db',
+    });
+}
 
 connection.connect(function(err) {
     if (err) {
